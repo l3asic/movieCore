b5afa4f5523aa47f88543c3c65632441366e22dd
l3jh <jhcx7129@naver.com>
2023-11-21 16:09:05 +0900

    프로젝트 첫 세팅

---
+++ src/main/java/com/example/movieCore/MovieCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCoreApplication.class, args);
+	}
+
+}
