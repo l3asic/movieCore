1d965fa7e62c2aa720e7828eba4129dfeea82f5f
l3jh <jhcx7129@naver.com>
2023-11-29 03:54:14 +0900

    영화 모듈 - 영화 목록 설계 개발

--- src/main/reactfront/src/views/movie/MovieList.js
+++ src/main/reactfront/src/views/movie/MovieList.js
@@ -1,12 +1,103 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 
+import {
+  CRow,
+  CCol,
+  CButton,
+  CCard,
+  CCardBody,
+  CCardImage,
+  CCardText,
+  CCardTitle
+} from '@coreui/react';
+
+import ReactImg from "../../assets/images/react.jpg"
+
 function MovieList(){
+
+
+
+
   return(
     <>
-      <h1>영화 리스트 페이지 입니다</h1>
-      <h1>영화 리스트 페이지 입니다</h1>
-      <h1>영화 리스트 페이지 입니다</h1>
+      <h3>영화 리스트 페이지 입니다</h3>
+
+      {/** 첫번째 줄 (3개 아이템)  */}
+
+      <CRow className="mt-5">
+        <CCol sm="4">
+          <CCard style={{ width: '18rem' }}>
+            <CCardImage orientation="top" src={ReactImg} />
+            <CCardBody>
+              <CCardTitle>Card title</CCardTitle>
+              <CCardText>텍스트</CCardText>
+              <CButton href="#">Go somewhere</CButton>
+            </CCardBody>
+          </CCard>
+        </CCol>
+
+        <CCol sm="4">
+          <CCard style={{ width: '18rem' }}>
+            <CCardImage orientation="top" src={ReactImg} />
+            <CCardBody>
+              <CCardTitle>Card title</CCardTitle>
+              <CCardText>텍스트</CCardText>
+              <CButton href="#">Go somewhere</CButton>
+            </CCardBody>
+          </CCard>
+        </CCol>
+
+        <CCol sm="4">
+          <CCard style={{ width: '18rem' }}>
+            <CCardImage orientation="top" src={ReactImg} />
+            <CCardBody>
+              <CCardTitle>Card title</CCardTitle>
+              <CCardText>텍스트</CCardText>
+              <CButton href="#">Go somewhere</CButton>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+
+
+      {/** 두번쨰 줄 */}
+      <CRow className="mt-5">
+        <CCol sm="4">
+          <CCard style={{ width: '18rem' }}>
+            <CCardImage orientation="top" src={ReactImg} />
+            <CCardBody>
+              <CCardTitle>Card title</CCardTitle>
+              <CCardText>텍스트</CCardText>
+              <CButton href="#">Go somewhere</CButton>
+            </CCardBody>
+          </CCard>
+        </CCol>
+
+        <CCol sm="4">
+          <CCard style={{ width: '18rem' }}>
+            <CCardImage orientation="top" src={ReactImg} />
+            <CCardBody>
+              <CCardTitle>Card title</CCardTitle>
+              <CCardText>텍스트</CCardText>
+              <CButton href="#">Go somewhere</CButton>
+            </CCardBody>
+          </CCard>
+        </CCol>
+
+        <CCol sm="4">
+          <CCard style={{ width: '18rem' }}>
+            <CCardImage orientation="top" src={ReactImg} />
+            <CCardBody>
+              <CCardTitle>Card title</CCardTitle>
+              <CCardText>텍스트</CCardText>
+              <CButton href="#">Go somewhere</CButton>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+
+
     </>
 
   ) // return
