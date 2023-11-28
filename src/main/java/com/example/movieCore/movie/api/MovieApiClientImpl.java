eda581f70ba8aa9567a1c2c3102ad661adb2e74f
l3jh <jhcx7129@naver.com>
2023-11-29 05:29:55 +0900

    Revert "영화 모듈 - 영화 회사 api 호출 설계 개발"
    This reverts commit 570dd0a679c65521bdcf07443c2ee1ba3ba7a40b.

--- src/main/java/com/example/movieCore/movie/api/MovieApiClientImpl.java
+++ src/main/java/com/example/movieCore/movie/api/MovieApiClientImpl.java
@@ -13,7 +13,7 @@ import java.util.*;
 
 
 @Component
-public class MovieApiClient {
+public class MovieApiClientImpl{
     /** 발급받은 API 키 */
     private String key = "7ed99cc4e7bce9910e15252a08c4ec17";
 
@@ -109,50 +109,6 @@ public class MovieApiClient {
         movieInfoBean = dataConverter.extractMovieInfoBean(jsonMap);
 
         return movieInfoBean;
-
-
-    }
-
-
-    /** 영화 회사 호출 */
-    public MovVo callMovieCompanyApi(){
-
-        // API 엔드포인트 URL
-        String apiUrl = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/company/searchCompanyList.json";
-
-
-        // WebClient 인스턴스 생성
-        WebClient webClient = WebClient.create();
-
-        // API 호출 URL 및 파라미터 조합
-        String fullUrl = String.format("%s?key=%s", apiUrl, key);
-
-        // API 호출 및 응답 받기
-        String responseBody = webClient.get()
-                .uri(fullUrl)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-
-        // JSON 데이터를 객체로 변환
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> jsonMap = new HashMap<>();
-        try {
-            jsonMap = objectMapper.readValue(responseBody, new TypeReference<>() {
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-//        DataConverter dataConverter = new DataConverter();
-//        MovieInfoBean movieInfoBean;
-//
-//        movieInfoBean = dataConverter.extractMovieInfoBean(jsonMap);
-
-        MovVo movVo = new MovVo();
-
-        return movVo;
 
 
     }
