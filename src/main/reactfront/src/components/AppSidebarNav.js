40bec3bd7ad6b6fa1a38d26af4f00ae5787ed679
l3jh <jhcx7129@naver.com>
2024-03-19 02:45:07 +0900

    사이드 네비 - 폴더 리스트 렌더링 2차 개발 (최조 접근시 미렌더링 픽스)

--- src/main/reactfront/src/components/AppSidebarNav.js
+++ src/main/reactfront/src/components/AppSidebarNav.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import { CBadge } from '@coreui/react'
+import { CBadge, CNavGroup, CNavItem } from '@coreui/react';
+import axios from 'axios';
+import CIcon from '@coreui/icons-react';
+import { cilNotes } from '@coreui/icons';
 
 export const AppSidebarNav = ({ items }) => {
-  const location = useLocation()
+  const location = useLocation();
+
   const navLink = (name, icon, badge) => {
     return (
       <>
@@ -17,8 +21,57 @@ export const AppSidebarNav = ({ items }) => {
           </CBadge>
         )}
       </>
-    )
-  }
+    );
+  };
+
+  const [navItems, setNavItems] = useState(items);
+
+  useEffect(() => {
+    const selectAllFolderBoardList = async () => {
+      try {
+        const res = await axios.post('/selectAllFolderBoardList');
+        const brdVo = res.data.brdVo;
+        const folderList = brdVo.folderBeanList;
+
+        const updatedItems = [...items];
+
+        for (let i = 0; i < folderList.length; i++) {
+          const folderBean = folderList[i];
+          const boardList = folderList[i].boardBeanList;
+
+          const boardItemList = [];
+
+          for (let j = 0; j < boardList.length; j++) {
+            const boardBean = boardList[j];
+
+            const boardItem = {
+              component: CNavItem,
+              name: boardBean.brdName,
+              to: '/base/accordion',
+            };
+
+            boardItemList.push(boardItem);
+          }
+
+          const folderItem = {
+            component: CNavGroup,
+            name: folderBean.folName,
+            to: '/base',
+            icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
+            items: boardItemList,
+          };
+
+          updatedItems.splice(6 + i, 0, folderItem);
+        }
+
+        setNavItems(updatedItems);
+      } catch (err) {
+        alert('등록 실패 (오류)');
+      }
+    };
+
+    selectAllFolderBoardList();
+  }, []); // Call selectAllFolderBoardList only once on component mount
 
   const navItem = (item, index) => {
 
@@ -27,8 +80,8 @@ export const AppSidebarNav = ({ items }) => {
       return null;
     }
 
-    const { component, name, badge, icon, ...rest } = item
-    const Component = component
+    const { component, name, badge, icon, ...rest } = item;
+    const Component = component;
     return (
       <Component
         {...(rest.to &&
@@ -60,7 +113,7 @@ export const AppSidebarNav = ({ items }) => {
         {...rest}
       >
         {item.items?.map((item, index) =>
-          item.items ? navGroup(item, index) : navItem(item, index),
+          item.items ? navGroup(item, index) : navItem(item, index)
         )}
       </Component>
     )
@@ -68,8 +121,10 @@ export const AppSidebarNav = ({ items }) => {
 
   return (
     <React.Fragment>
-      {items &&
-        items.map((item, index) => (item.items ? navGroup(item, index) : navItem(item, index)))}
+      {navItems &&
+        navItems.map((item, index) =>
+          item.items ? navGroup(item, index) : navItem(item, index)
+        )}
     </React.Fragment>
   )
 }
